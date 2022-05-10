88d5b5aa50595d5556dc63ecceee91820395318e
Gabriel Machado <gabrielmjj5@gmail.com>
2022-05-10 17:35:41 -0300

    Routes and Database

---
+++ src/controllers/QuestionController.js
@@ -0,0 +1,10 @@
+module.exports = {
+    index(req, res){
+        const roomId = req.params.room
+        const questionID = req.params.question
+        const action = req.params.action
+        const password = req.body.password
+
+        console.log(`room = ${roomId}, questionId = ${questionID}, action = ${action}, password = ${password}`)
+    }
+}
